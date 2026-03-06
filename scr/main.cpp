ae42ee7055e59d8ed13c7df8fbb166773a5a23d4
gaia <gayathiri.ravendirane@gmail.com>
2026-03-06 21:28:42 +0100

    position.hpp done

--- scr/main.cpp
+++ scr/main.cpp
@@ -9,6 +9,23 @@
 // #include "position_test.cpp" ERROR !!! ask the teacher how it works and i have forgotten
 
 
+/*
+TODO
+
+  A B C D E F G H
+8 r h e m k e h r
+7 p p p p p p p p
+6 . . . . . . . .
+5 . . . . . . . .
+4 . . . . . . . .
+3 . . . . . . . .
+2 P P P P P P P P
+1 R H E M K E H R
+
+White move:
+> D3
+*/
+
 int main()
 {
     std::cout << "+==============================================================+" << std::endl;
